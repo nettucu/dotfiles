ef97d33665c7f912b732ddc552b47b92be4f1c65
Catalin Trifu <ctrifu@gmail.com>
2025-01-26 14:23:05 +0200

    changed from ~ to /home/catalin in path.sh

--- shell/path.sh
+++ shell/path.sh
@@ -1,13 +1,13 @@
-path_prepend "~/bin"
+path_prepend "${HOME}/bin"
 path_prepend /opt/android-sdk-linux/tools
 path_prepend /opt/android-sdk-linux/platform-tools
-path_prepend "~/.dotnet/tools"
+path_prepend "${HOME}/.dotnet/tools"
 
-for d in $( \find /opt/scripts -type f -executable ! \( -path '**/.git/*' -o -path '**/conf/*' -o -path '**/cron*' -o -path '**/bcc/*' \) -printf '%h\n' | uniq ); do
-    path_prepend "${d}"
+for d in $(\find /opt/scripts -type f -executable ! \( -path '**/.git/*' -o -path '**/conf/*' -o -path '**/cron*' -o -path '**/bcc/*' \) -printf '%h\n' | uniq); do
+  path_prepend "${d}"
 done
 export PATH
 
 if [[ -f /opt/google-cloud-cli/path.zsh.inc ]]; then
-    source /opt/google-cloud-cli/path.zsh.inc
+  source /opt/google-cloud-cli/path.zsh.inc
 fi
