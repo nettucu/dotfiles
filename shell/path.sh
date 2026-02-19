668642687ed2436e97a22d2da81fa69f7142ea33
Catalin Trifu <ctrifu@gmail.com>
2026-02-19 21:54:50 +0200

    fix: added tests for non-existent paths

--- shell/path.sh
+++ shell/path.sh
@@ -1,11 +1,15 @@
 path_prepend "${HOME}/bin"
-path_prepend /opt/android-sdk-linux/tools
-path_prepend /opt/android-sdk-linux/platform-tools
+path_prepend "{$HOME}/.local/bin"
+[[ -d /opt/android-sdk-linux ]] && path_prepend /opt/android-sdk-linux
+[[ -d /opt/android-sdk-linux/tools ]] && path_prepend /opt/android-sdk-linux/tools
+[[ -d /opt/android-sdk-linux/platform-tools ]] && path_prepend /opt/android-sdk-linux/platform-tools
 path_prepend "${HOME}/.dotnet/tools"
 
-for d in $(\find /opt/scripts -type f -executable ! \( -path '**/.git/*' -o -path '**/conf/*' -o -path '**/cron*' -o -path '**/bcc/*' \) -printf '%h\n' | uniq); do
-  path_prepend "${d}"
-done
+if [[ -d /opt/scripts ]]; then
+  for d in $(\find /opt/scripts -type f -executable ! \( -path '**/.git/*' -o -path '**/conf/*' -o -path '**/cron*' -o -path '**/bcc/*' \) -printf '%h\n' | uniq); do
+    path_prepend "${d}"
+  done
+fi
 export PATH
 
 if [[ -f /opt/google-cloud-cli/path.zsh.inc ]]; then
