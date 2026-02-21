21a80e32690a8c68f39947b6927fd1bbbb4afc85
Catalin Trifu <ctrifu@gmail.com>
2026-02-21 14:24:53 +0200

    chore: neovim cleanup

--- nvim/lazyvim/lua/plugins/avante.lua
+++ nvim/lazyvim/lua/plugins/avante.lua
@@ -28,10 +28,9 @@ return {
     "MunifTanjim/nui.nvim",
     --- The below dependencies are optional,
     "nvim-mini/mini.pick", -- for file_selector provider mini.pick
-    "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
     "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
     "ibhagwan/fzf-lua", -- for file_selector provider fzf
-    "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+    "nvim-mini/mini.icons", -- or echasnovski/mini.icons
     {
       -- support for image pasting
       "HakonHarnes/img-clip.nvim",
